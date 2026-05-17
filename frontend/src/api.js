a4ab360414a8fd7afc733eda3d994eb385afd7a9
Anuroop0333 <anuroopjajoba3@gmail.com>
2026-05-17 18:24:13 -0400

    add ngrok skip browser warning header

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,10 @@ import axios from 'axios'
 
 const API_BASE = import.meta.env.VITE_API_URL || 'http://localhost:8000'
 
-const api = axios.create({ baseURL: API_BASE })
+const api = axios.create({
+  baseURL: API_BASE,
+  headers: { 'ngrok-skip-browser-warning': 'true' },
+})
 
 // Attach JWT token to every request
 api.interceptors.request.use((config) => {
